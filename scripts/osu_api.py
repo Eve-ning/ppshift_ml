8048eece2547f55f10a2d54a59a056fbe57a3b26
Eve-ning <johnchangqi@hotmail.com>
2019-02-28 23:19:43 +0800

    Decode content in get_replay and strip useless data

--- scripts/osu_api.py
+++ scripts/osu_api.py
@@ -8,6 +8,8 @@ Created on Thu Feb 28 21:52:55 2019
 import requests
 import time
 import json
+import base64
+import lzma
 
 # GLOBAL VAR
 base_api_url = "https://osu.ppy.sh/api/"
@@ -40,6 +42,27 @@ def get_replay(beatmap_id: int, mode: int, user: int):
     response = requests.get(api_url, params = param)
     json_data = json.loads(response.text)
     
+    # We will decode the content here
+    # But we check the status code before proceeding
     time.sleep(6) # Pause
     
-    return json_data, response.status_code
+    # If response is not 200
+    if (response.status_code != 200):
+        return "bad request", response.status_code
+    
+    decoded = base64.b64decode(json_data["content"])
+    decompressed = str(lzma.decompress(decoded))[:-2]
+    
+    # We split it into a nested list
+    output = [x.split("|") for x in decompressed.split(",")]
+    
+    # Drop the last 2 useless elements    
+    [x.pop(2) for x in output]
+    [x.pop(2) for x in output]
+    
+    return output, response.status_code
+
+#cont, code = get_replay(1837187, 3, 2193881)
+#
+#print(code)
+#print(cont)
