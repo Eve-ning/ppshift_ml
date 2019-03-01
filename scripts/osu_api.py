593f379e72667bdcd46810220a70bac5dfd1c57e
Eve-ning <johnchangqi@hotmail.com>
2019-03-01 19:49:43 +0800

    Update osu_api with replay converter

--- scripts/osu_api.py
+++ scripts/osu_api.py
@@ -10,6 +10,7 @@ import time
 import json
 import base64
 import lzma
+import operator
 
 # GLOBAL VAR
 base_api_url = "https://osu.ppy.sh/api/"
@@ -52,6 +53,7 @@ def get_replay(beatmap_id: int, mode: int, user: int):
     
     decoded = base64.b64decode(json_data["content"])
     decompressed = str(lzma.decompress(decoded))[:-2]
+    decompressed = decompressed[2:]
     
     # We split it into a nested list
     output = [x.split("|") for x in decompressed.split(",")]
@@ -62,7 +64,79 @@ def get_replay(beatmap_id: int, mode: int, user: int):
     
     return output, response.status_code
 
-#cont, code = get_replay(1837187, 3, 2193881)
-#
-#print(code)
-#print(cont)
+def convert_replay_data(replay_data: list):
+    
+    # Eg. 1 3 4 (bin: 25) will output
+    # [1,0,1,1,0,0,0,0,0]   
+    def decode_key_action(action: int):
+        
+        # Convert action to string List
+        action_str = list(bin(action)[:1:-1])
+        
+        # Find how many missing elements and append them
+        action_missing = 9 - len(action_str)
+        action_str.extend(['0'] * action_missing)
+        
+        # Convert string List to int List
+        action_int = list(map(int, action_str))
+        
+        # Add the list together to ensure 9 element cases
+        return action_int
+    
+    # [0,0,1,0,0,1,0,0,0] prev
+    # [0,1,0,0,0,1,0,0,0] curr
+    # [0,1,-1,0,0,0,0,0,0] output
+    def get_action_difference(prev_flag: list, curr_flag: list):
+        
+        # Gets the difference between curr and prev
+        diff_flag = list(map(operator.sub, curr_flag, prev_flag))
+        
+        # 1:  1 - 0 <Key Pressed>
+        # 0:  0 - 0 | 1 - 1 <No Change>
+        # -1: 0 - 1 <Key Released>
+        
+#        print('prev: {}'.format(prev_flag))
+#        print('curr: {}'.format(curr_flag))
+#        print('diff: {}'.format(diff_flag))
+        
+        # We are required to + 1 as -0 and +0 are the same
+        # So columns will always start from 1 to 9
+        key_p = [(i + 1) for i, y in enumerate(diff_flag) if y == 1]
+        key_r = [-(i + 1) for i, y in enumerate(diff_flag) if y == -1]
+        
+        output = []
+        output.extend(key_p)
+        output.extend(key_r)
+        return output
+            
+    timestamp = 0
+    prev_action = [0] * 9
+    curr_action = [0] * 9
+    
+    output = []
+    
+    for step in replay_data:
+
+        timestamp += int(step[0])
+        
+        curr_action = decode_key_action(int(step[1]))
+        
+        diff_action = get_action_difference(prev_action, curr_action)
+        
+        # Skip if there's no change in action
+        if (len(diff_action) == 0):
+#            print('--skip--')
+            continue
+#        print ('diff: {}'.format(diff_action))
+        # For each key press/release, we append to output as a separate action
+        if (timestamp > 0):
+            for key in diff_action:
+                output.append([timestamp, key])
+        
+        prev_action = curr_action
+  
+    print('otpt: {}'.format(output))
+
+#cont, code = get_replay(1217397, 3, 4728433)
+#convert_replay_data(cont)
+
