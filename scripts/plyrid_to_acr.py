45acf98a4e5186592b0a36a3cace8097c367d391
Eve-ning <johnchangqi@hotmail.com>
2019-03-12 21:56:52 +0800

    Adjust plyrid_to_acr to new interface

--- scripts/plyrid_to_acr.py
+++ scripts/plyrid_to_acr.py
@@ -8,9 +8,8 @@ Created on Sun Mar  3 09:19:13 2019
 import api_main
 import time
 import osuho_to_acd
-import pandas
 
-def run(player_id_list: list, beatmap_id: int, keys: int) -> pandas.DataFrame:
+def run(player_id_list: list, beatmap_id: int, keys: int):
     
     replay_list = []
     status_count = 0
@@ -18,10 +17,11 @@ def run(player_id_list: list, beatmap_id: int, keys: int) -> pandas.DataFrame:
     
     player_id_it = 0
     
+    print("BM:" + str(beatmap_id), end="\t") 
+    
     while player_id_it < len(player_id_list):
             
         replay,status_code = api_main.get_replay(beatmap_id, 3, player_id_list[player_id_it])
-        print ("BM:" + str(beatmap_id) + "\tPL:" + str(player_id_it + 1) + "/50")
         
         if (status_code == 200):
             # We will need to convert all columns to actions
@@ -32,37 +32,45 @@ def run(player_id_list: list, beatmap_id: int, keys: int) -> pandas.DataFrame:
                 offset = pair[0]
                 column = pair[1]
                 
+                # There's an issue where replays will have really weird keys
+                # Only at the very start
+                if (abs(column) > keys and offset < 100):
+                    continue
+                
                 # This is required as we cannot convert with a negative column
                 is_release = column < 0 
                 action = osuho_to_acd.column_to_action[keys][abs(column) - 1]
                 action = -action if is_release else action
                 
-                replay_n.append([offset, action, player_id_list[player_id_it], player_id_it])
+                replay_n.append([offset, action])
             
             # Reset Count on Good input
             status_count = 0
             player_id_it += 1
-            replay_list.extend(replay_n)
+            replay_list.append(replay_n)
+            print('O', end="")
             
         elif (status_code == 0):
             # When the replay is corrupt we append a dummy value
             replay_list.append([0, 0])
-            print("Bad Replay, appending dummy value.")
             player_id_it += 1
+            print('X', end="")
             
         else:
             # Count up Status Errors 
             status_count += 1
             # Bad GET, we will wait 3 more seconds
-            print ("Bad Status: " + str(status_code))
+            print()
+            print("Bad Status: " + str(status_code))
             time.sleep(3)
             
             if (status_count == status_break):
-                print ("Bad Status Threshold Exceeded")
+                print("Bad Status Threshold Exceeded")
                 return None
+    
+    print()
          
-    replay_df = pandas.DataFrame(replay_list, columns=['offset', 'action', 'player_id', 'player_id_it'])       
-    return replay_df
+    return replay_list
         
         
 
